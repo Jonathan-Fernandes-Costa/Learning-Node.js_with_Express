8a74c2b5e23a03db477ed5d7313e33663d2d9270
Jonathan Fernandes <jonathanfernandes638@gmail.com>
2023-01-11 09:30:59 -0300

    Organizando codigo com a arquitetura MVC

--- src/app.js
+++ src/app.js
@@ -2,30 +2,22 @@
 import express from "express";
 import db from "./config/dbConnect.js"
 import livros from "./models/Livro.js"
+import routes from "./routes/index.js"
 db.on("error", console.log.bind(console, 'Erro de conexão com o banco de dados'))//interligando banco de dados
 db.once("open", ()=>{console.log("Conexão com o banco de dados estabelecida")
 })
 const app = express()
 app.use(express.json())
 //CRUD
-//Acessar dados
-app.get('/', (req,res) => {
-    res.status(200).send("Curso de Node");
-})
-app.get('/livros', (req,res) =>{
-    livros.find((err, livros) =>{res.status(200).json(livros)})
-    
-})
+//Acessar dado
+routes(app)
 app.get('/livros/:id', (req, res) =>{
     let {id} = req.params;
     let index = Buscalivro(id)
     res.status(200).json(livros[index])
 })
 //Adicionar Dados
-app.post('/livros', (req, res) => {
-    livros.push(req.body)
-    res.status(201).send("Livro adicionado")
-})
+
 //Atualizar dados
 app.put('/livros/:id', (req, res) =>{
     let {id} = req.params;
