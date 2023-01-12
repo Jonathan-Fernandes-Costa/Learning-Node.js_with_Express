1cba2ef1705fb78b429ade147c48895f80ff002b
Jonathan Fernandes <108033810+Jonathan-Fernandes-Costa@users.noreply.github.com>
2023-01-12 08:04:30 -0300

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 mongoose.set("strictQuery", true);
-mongoose.connect("mongodb+srv://Jojonick:projeto123@projeto-node.2omkwsm.mongodb.net/curso-node");
+mongoose.connect("mongodb+srv://*******:******@projeto-node.2omkwsm.mongodb.net/curso-node");
 
 let db = mongoose.connection;
 export default db
